664745a8009ef5e79cb56cd674ed386b85197b84
LAPTOP-R0HO03P8\daaru <arihasan707@gmail.com>
2024-06-26 11:42:23 +0700

    membuat relasi one-to-many

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
 {
@@ -14,4 +16,9 @@ class Task extends Model
         'deskripsi',
         'status'
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
